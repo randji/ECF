4eb9e8b3e4c87b49d2802d456e912add8863cd98
randji <stephanesoilihi@yahoo.fr>
2023-11-17 00:52:43 +0100

    page index

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Garage V.Parrot</title>
+</head>
+<body>
+    <header>
+        <div class="bacground">
+            <p>V. Parrot</p>
+            <img class="menuBurger" src="menuBurger.png" alt="menu burger">
+        </div>
+    </header>
+    <main>
+        <img src="LOGO_Vparrot.png" alt="logo">
+        <img src="daa9c2eb-79ca-4207-b92a-0d7c47e6a1fb.webp" alt="voiture">
+    </main>
+    <footer>
+        <div class="background">
+            <p>tel: 01 12 25 45 74</p>
+            <p>horaire</p>
+            <p>lundi au samedi : 9h-18h</p>
+            <p>dimanche : Fermé</p>
+            <form action="">Formulaire de contact</form>
+        </div>
+    </footer>
+</body>
+</html>
