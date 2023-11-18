abceec502b4bb308be7eae2b5f7054b1f54c7874
randji <stephanesoilihi@yahoo.fr>
2023-11-18 22:53:57 +0100

    1er css

--- index.php
+++ index.php
@@ -3,18 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_index.css">
     <title>Garage V.Parrot</title>
 </head>
 <body>
     <header>
-        <div class="bacground">
+        <div class="background">
             <p>V. Parrot</p>
-            <img class="menuBurger" src="menuBurger.png" alt="menu burger">
+            <img class="menuBurger" src="img/menuBurger.png" alt="menu burger">
         </div>
     </header>
     <main>
-        <img src="LOGO_Vparrot.png" alt="logo">
-        <img src="daa9c2eb-79ca-4207-b92a-0d7c47e6a1fb.webp" alt="voiture">
+        <img class="logo" src="img/LOGO_Vparrot.png" alt="logo">
+        <img class="voitureFond" src="img/daa9c2eb-79ca-4207-b92a-0d7c47e6a1fb.webp" alt="voiture">
     </main>
     <footer>
         <div class="background">
