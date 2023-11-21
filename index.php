b7920b0b6a6c605892dabd6c3c5c06460c1a7c80
randji <stephanesoilihi@yahoo.fr>
2023-11-22 00:38:07 +0100

    ajouter lien service

--- index.php
+++ index.php
@@ -3,16 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Amiko&family=Cardo&family=Mukta+Vaani:wght@800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style_index.css">
     <title>Garage V.Parrot</title>
 </head>
 <body class="grid-container">
+    
     <header class="background">
        
-        <p class="titre">V. Parrot</p>
-        <img class="menuBurger" src="img/menuBurger.png" alt="menu burger">
-            </div>
-        </div>
+        <p class="titre">V. PARROT</p>
+         
     </header>
 
     <main>
@@ -21,16 +23,23 @@
         <img class="fond" src="img/voitureFond.png" alt="voiture">
         <p class="slogan">“Laissez votre voiture <br> 
         entre de bonnes mains”</p>
+        <div class="service">
+            <div class="reparation">Réparation carrosserie</div>
+            <div class="mecanique">Mécanique des voitures</div>
+            <div class="entretien">Entretien régulier</div>
+            <div class="vente">Vente de véhicules d'occasion</div>
+        </div>
+       
     </main>
 
     <footer class="background">
     
             <p class="tel">tel: 01 12 25 45 74</p>
             <div class="horaire">
-                <p style="font-size: 15px;"><u>HORAIRE</u></p>
-                <p>lundi au samedi : 9h-18h <br>dimanche : Fermé</p>
+                <p style="font-size: 15px; font-family: 'Mukta Vaani', sans-serif; color: #eba83a;"><u>HORAIRE</u></p>
+                <p style="font-family: 'Mukta Vaani', sans-serif;">lundi au samedi : 9h-18h <br>dimanche : Fermé</p>
             </div>
-            <form class="formulaire" action="">Formulaire de contact</form>
+            <form class="formulaire" action="" >Formulaire de contact</form>
         
     </footer>
 </body>
